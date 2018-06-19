028e6e30f4170d0218b532a5238ebf5066e1bc4b
Robert Tuohy <Robert.Tuohy@UKA0546.local>
2018-06-19 11:17:05 +0100

    optimisation attempts

--- src/components/weather-form.js
+++ src/components/weather-form.js
@@ -2,16 +2,6 @@ import React from 'react'
 import MagGlass from '../images/searchmagnify.png'
 import styled from 'styled-components'
 
-const TableHeader = styled.h1`
-    text-align:center;
-    font-family: Tahoma;
-`
-
-const TableHeader2 = styled.h2`
-    text-align:center;
-    font-family: Tahoma;
-    `
-
 const StyledForm = styled.div`
     text-align: center;
 `
@@ -20,8 +10,6 @@ export default (props) => {
   const {handleSubmit, value, handleChange} = props
   return (
     <StyledForm>
-      <TableHeader>Enter a UK City</TableHeader>
-      <TableHeader2>Receive a 10 day forecast</TableHeader2>
       <form onSubmit={handleSubmit}>
         <input type='text' aria-label='Search Box' name='city' placeholder='Enter City' autoComplete='off' value={value} onChange={handleChange} />
         <button type='submit' aria-label='Search Button'>
